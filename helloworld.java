c0cecc7881de962b15a452a1ec3207a58b15d4dd
Santosh <xxv@gnam.com>
2019-06-14 15:22:21 -0400

    fifth commit

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,6 @@
-//this program will print hello world
-class HelloWorld
+c class HelloWorld
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		
-		// prints welcome msg in the window
-		System.out.pintLn("Welcome to git!");
 	}
 }
